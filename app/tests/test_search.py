6f484f03eee049ff1126b671d67b57ddb9191ca3
ricaportela <ricaportela@gmail.com>
2016-09-30 11:07:09 -0300

    Creating test cases

--- app/tests/test_search.py
+++ app/tests/test_search.py
@@ -3,12 +3,12 @@ import pytest
 from app.aplica import app
 
 @pytest.mark.usefixtures('client_class')
-class testApp:
+class TestApp:
     @pytest.fixture
     def app(self):
         return app
 
     def test_search(self):
         responde = self.client.get("/search/wordcounts/")
-        assert response.data.decode('utf-8') == "<h1> Testando... </h1>"
+        assert response.data.decode('utf-8') == "<h1> Testando com palavra... </h1>"
         assert response.status_code == 200
