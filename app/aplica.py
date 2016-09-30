2d1fe66495545406fc99badeea250947b1e51a3e
ricaportela <ricaportela@gmail.com>
2016-09-30 14:41:08 -0300

    Creating test case

--- app/aplica.py
+++ app/aplica.py
@@ -5,29 +5,27 @@ app = Flask(__name__)
 
 wordcounts = [
     {
-        'id': 1,
         'keyword': u"Microfocus",
         'value': 45
     },
     {
-        'id': 2,
         'keyword': u"Programador",
         'value': 33
     },
     {
-        'id': 34,
         'keyword': u"Cobol",
         'value': 3
     },
 ]
 
 
-@app.route("/search/wordcounts/<keyword>", methods=['GET'])
+@app.route("/search", methods=['GET'])
 def search_keyword():
     query = request.args.get('query', '')
     for item in wordcounts:
         if item['keyword'] == query:
             return jsonify({'wordcounts': [item]})
+    return jsonify({})
 
 if __name__ == "__main__":
     app.run(debug=True)
